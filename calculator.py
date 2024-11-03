41ed43dfa500ddfa7258aff7ce2c2e5081bea710
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-03 11:24:18 -0800

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,91 @@
+operators = {"^": 4, "*": 3, "/": 3, "+": 2, "-": 2}
+
+def is_number(string):
+  try:
+    float(string)
+    return True
+  except ValueError:
+    return False
+  
+def is_operator(string):
+  return string in operators.keys()
+
+def is_greater(a, b):
+  # compares the precedence of the two operators
+  return operators[a] > operators[b]
+
+def top_of(stack):
+  return stack[-1] if stack else None
+
+def shunt_yard(expression: str):
+  # Convert an expression into reverse Polish notation
+  # with the shunting yard algorithm
+
+  input = []
+  stack = []
+  output = []
+  
+  # strip spaces and then convert to tokens
+  for i in "".join(expression.split()):
+    if is_number(i) or i == ".":
+      if len(input)>0:
+        if is_number(input[-1]) or input[-1] == ".":
+          input[-1] += i
+          continue
+      input.append(i)
+    elif i in "()" or is_operator(i):
+      input.append(i)
+    else:
+      input.append(i)
+  print(input)
+
+  for thing in input:
+    if is_number(thing):
+      output.append(float(thing))
+    elif is_operator(thing):
+      while not (top_of(stack) == None or top_of(stack) in "()") and is_greater(top_of(stack), thing):
+        output.append(stack.pop())
+      stack.append(thing)
+    elif thing == "(":
+      stack.append(thing)
+    elif thing == ")":
+      while not (top_of(stack) == None or top_of(stack) == "("):
+        output.append(stack.pop())
+      stack.pop()
+  while len(stack) > 0:
+    output.append(stack.pop())
+  return output
+
+def evaluate(expression: str):
+  # Evaluate an expression
+
+  output = shunt_yard([x for x in expression])
+  stack = []
+
+  while len(output) > 0:
+    if is_number(output[0]):
+      stack.append(output.pop(0))
+    elif is_operator(output[0]):
+      if len(stack)==0: # no numbers left in stack
+        return "invalid expression" # no numbers error 2
+      a = stack.pop()
+      if len(stack)==0: # only one number left in stack
+        return "invalid expression" # no numbers error 1
+      b = stack.pop()
+      opr = output.pop(0)
+      if opr == "^":
+        stack.append(b ** a)
+      elif opr == "*":
+        stack.append(b * a)
+      elif opr == "/":
+        try:
+          stack.append(b / a)
+        except ZeroDivisionError:
+          return "invalid expression" # division by zero error
+      elif opr == "+":
+        stack.append(b + a)
+      elif opr == "-":
+        stack.append(b - a)
+  return stack[0] if stack else "invalid expression" # you entered nothing error
+
+print(evaluate(shunt_yard(input("Enter expression: "))))
---
+++ main.py
@@ -0,0 +1,35 @@
+# DuckBot
+from dotenv import load_dotenv
+import os
+import discord
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  guilds = '\n - '.join([f'{guild.name} (id: {guild.id})' for guild in client.guilds])
+  print(f'{client.user} is active in the following guilds:')
+  print(f' - {guilds}\n')
+  me = client.get_user(587040390603866122)
+  await me.send('DuckBot is online')
+  print('DuckBot sent a DM to doduodrio (id: 587040390603866122) upon activating!')
+
+@client.event
+async def on_message(message):
+  # if it's your own message, don't react
+  if message.author == client.user:
+    return
+  # otherwise, react
+  msg = message.content.lower() # "hElLo!" => "hello!"
+
+  if 'hello' in msg:
+    await message.channel.send('hello')
+    print('DuckBot said hello')
+
+client.run(TOKEN)
