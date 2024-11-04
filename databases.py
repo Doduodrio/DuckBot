be43a69160c69c24443cfab9db3751cc7413c945
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-04 00:52:43 -0800

    Add raw_content (content as a list, not dict)

--- databases.py
+++ databases.py
@@ -5,13 +5,11 @@ class Database:
     def __init__(self, url, key):
         self.url = url
         self.content = {}
+
         data = requests.get(self.url)
         data.encoding = 'utf-8'
         file = StringIO(data.text)
-        file_list = file.readlines()
-        for i in file_list:
-            line = i.split('\t')
-            self.content[line[key].lower().strip('-')] = line
+        self.raw_content = file.readlines()
         
     def get(self, name):
         return self.content[name.lower()]
