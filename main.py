0a07a55ee5d48b78f7fa999d63ad5af9405ae43f
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-03 17:45:28 -0800

    Split msg by whitespace chars and add %roll

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # DuckBot
+
 from dotenv import load_dotenv
 import os
 import discord
 
 from calc import evaluate
+from roll import roll
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
@@ -28,31 +30,42 @@ async def on_message(message):
   if message.author == client.user:
     return
   # otherwise, react
-  msg = message.content.lower() # "hElLo!" => "hello!"
+  msg = message.content.lower().split() # "hElLo!" => "hello!"
 
-  if msg == "%ping":
+  # %ping: reply with pong
+  if msg[0] == '%ping':
     await message.channel.send('pong')
     print('\n' + 'DuckBot was pinged')
-  elif msg.startswith("%calc "):
-    expression = ''.join(msg.strip("%calc ").split())
+
+  # %calc: evaluate an expression
+  elif msg[0] == '%calc':
+    expression = ''.join(msg[1::])
     try:
       result = evaluate(expression)
       if int(result) == result:
         result = int(result)
     except:
       result = None
+    print('\n' + f'DuckBot evaluated:')
     try:
       float(result)
       await message.channel.send(f'```{expression} = {result}```')
-      print('\n' + f'DuckBot evaluated:')
       print('    ' + f'{expression} = {result}')
     except OverflowError:
       await message.channel.send(f'```number too large```')
-      print('\n' + f'DuckBot evaluated:')
       print('    ' + f'{expression} = number too large')
     except:
       await message.channel.send(f'```invalid expression```')
-      print('\n' + f'DuckBot evaluated:')
       print('    ' + f'{expression} = invalid expression')
+  
+  # %roll: return random numbers
+  elif msg == '%roll':
+    print('\n' + f'Duckbot rolled:')
+    if len(msg)==1:
+      await message.channel.send('Cannot roll nothing')
+      print('    ' + 'nothing')
+    else:
+      roll_msg = msg[1]
+      rolls = roll(roll_msg)
 
 client.run(TOKEN)
