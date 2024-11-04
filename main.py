1de3bb06634d7b703b2f187103ee570d87942761
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-03 18:27:50 -0800

    Add %r as an alias for %roll

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def on_message(message):
       print('    ' + f'{expression} = invalid expression')
   
   # %roll: return random numbers
-  elif msg[0] == '%roll':
+  elif msg[0] in ['%roll', '%r']:
     print('\n' + f'Duckbot rolled:')
     if len(msg)==1:
       await message.channel.send('cannot roll nothing')
