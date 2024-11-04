17d5e2b9e0437765f8cb887fa6b490f91d884712
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-03 21:08:58 -0800

    Rename get_pkmn_embed to get_pkmn

--- pokemon.py
+++ pokemon.py
@@ -4,7 +4,7 @@ import discord
 
 db = Database('https://docs.google.com/spreadsheets/d/1qIplFdrzRqHl91V7qRBtsb9LuC1TYW--TFoNlTDvpbA/export?format=tsv&gid=2042923402')
 
-def get_pkmn_embed(pkmn: str):
+def get_pkmn(pkmn: str):
     pokemon = db.get(pkmn)
     embed = discord.Embed(
         color = discord.Color.dark_teal(),
@@ -12,5 +12,5 @@ def get_pkmn_embed(pkmn: str):
         description = pokemon[2],
         timestamp = datetime.datetime.now()
     )
-    
+
     return embed
