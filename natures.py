2678d1b54060378b721b3c948717d375be359ae4
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-06 22:46:56 -0800

    Use tsv instead of Database object

--- natures.py
+++ natures.py
@@ -1,21 +1,27 @@
-from databases import Database
+from io import StringIO
 import datetime
 import discord
+import requests
 
 class Nature:
     def __init__(self, n):
-        self.name = n['Nature']
-        self.effect = n['Modifiers']
+        self.name = n[0]
+        self.effect = n[1]
 
-db = Database('https://docs.google.com/spreadsheets/d/1qIplFdrzRqHl91V7qRBtsb9LuC1TYW--TFoNlTDvpbA/export?format=csv&gid=54976138')
+# no using Database here because the NATURES sheet is formatted differently
+data = requests.get('https://docs.google.com/spreadsheets/d/1qIplFdrzRqHl91V7qRBtsb9LuC1TYW--TFoNlTDvpbA/export?format=tsv&gid=54976138')
+data.encoding = 'utf-8'
+file = StringIO(data.text)
+db_raw = file.readlines()
+db = {}
 
 # convert raw_content (list of dicts) to a dict of Nature
-for i in range(2, len(db.raw_content)):
-    n = db.raw_content[i]
-    db.content[n['Nature'].lower()] = Nature(n)
+for i in range(2, len(db_raw)):
+    line = db_raw[i].split('\t')
+    db[line[0].lower()] = Nature(line)
 
 def get_nature(nature: str):
-    n = db.get(nature.lower())
+    n = db[nature.lower()]
     embed = discord.Embed(
         color = discord.Color.dark_teal(),
         title = n.name,
