b126b9829a120d6b4d380613b119c1157cb4bf49
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-08 00:17:04 -0800

    Get type matchup for list of types instead of single type

--- type_chart.py
+++ type_chart.py
@@ -1,4 +1,4 @@
-type_chart = [
+TYPE_CHART = [
     [0, 0, 0, 0, 0, 1, 2, 2, 0, 1, 1, 0, 0, 0, 0, 2, 0, 0], # bug
     [2, 1, 0, 0, 2, 2, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0], # dark
     [0, 0, 2, 1, 2, 0, 1, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 1], # dragon
@@ -18,40 +18,44 @@ type_chart = [
     [1, 0, 1, 0, 1, 2, 2, 1, 0, 1, 2, 1, 1, 3, 1, 1, 1, 0], # steel
     [0, 0, 0, 2, 0, 0, 1, 0, 0, 2, 0, 1, 0, 0, 0, 0, 1, 1] # water
 ]
-types = ['bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water']
+TYPES = ['bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water']
 
 def get_matchup(type1, type2):
     # type matchup of type1 used on type2
-    return type_chart[types.index(type2)][types.index(type1)]
+    return TYPE_CHART[TYPES.index(type2)][TYPES.index(type1)]
 
-def defensive_matchup(type2):
+def defensive_matchup(types):
     # defensive type matchups of type2
-    resist = []
-    weak = []
-    immune = []
-    for type1 in types:
-        matchup = get_matchup(type1, type2)
-        if matchup == 1:
-            resist.append(type1)
-        elif matchup == 2:
-            weak.append(type1)
-        elif matchup == 3:
-            immune.append(type1)
-    return {'Resistances': resist, 'Weaknesses': weak, 'Immunities': immune}
+    output = {'bug': 0, 'dark': 0, 'dragon': 0, 'electric': 0, 'fairy': 0, 'fighting': 0, 'fire': 0, 'flying': 0, 'ghost': 0, 'grass': 0, 'ground': 0, 'ice': 0, 'normal': 0, 'poison': 0, 'psychic': 0, 'rock': 0, 'steel': 0, 'water': 0}
+    for type1 in TYPES:
+        for type2 in types:
+            matchup = get_matchup(type1, type2)
+            try:
+                if matchup == 1:
+                    output[type2] -= 1
+                elif matchup == 2:
+                    output[type2] += 1
+                elif matchup == 3:
+                    output[type2] = None
+            except:
+                break
+    return output
 
-def offensive_matchup(type1):
+def offensive_matchup(types):
     # offensive type matchups of type1
-    se = []
-    nve = []
-    immune = []
-    for type2 in types:
-        matchup = get_matchup(type1, type2)
-        if matchup == 1:
-            nve.append(type2)
-        elif matchup == 2:
-            se.append(type2)
-        elif matchup == 3:
-            immune.append(type2)
-    return {'Super Effective': se, 'Not Very Effective': nve, 'Ineffective': immune}
+    output = {'bug': 0, 'dark': 0, 'dragon': 0, 'electric': 0, 'fairy': 0, 'fighting': 0, 'fire': 0, 'flying': 0, 'ghost': 0, 'grass': 0, 'ground': 0, 'ice': 0, 'normal': 0, 'poison': 0, 'psychic': 0, 'rock': 0, 'steel': 0, 'water': 0}
+    for type2 in TYPES:
+        for type1 in types:
+            matchup = get_matchup(type1, type2)
+            try:
+                if matchup == 1:
+                    output[type2] -= 1
+                elif matchup == 2:
+                    output[type2] += 1
+                elif matchup == 3:
+                    output[type2] = None
+            except:
+                break
+    return output
 
-# note to future self: modify offensive and defensive matchup functions to accept a list of types
+# note to future self: also add functionality to look up a pokemon's type
