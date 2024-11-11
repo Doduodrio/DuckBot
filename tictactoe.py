7646971c3803b6829301d1e6c55b956c7db1daf1
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-11 01:16:11 -0800

    Add tictactoe buttons

--- tictactoe.py
+++ tictactoe.py
@@ -1,23 +1,23 @@
 import datetime
 import discord
 
-X = ':regional_indicator_x:' # or whatever the code is for emojis
+X = ':regional_indicator_x:' # or whatever the code is for discord emojis
 O = ':o2:'
 blank = ':black_large_square:'
-player, bot = 1, 2
+player, bot = X, O
 deepest = 99 # max recursions
 
-def move_piece(board, player, move):
+def make_move(board, symbol, move):
   # returns the new board state after making a move
   # board is the current board state
-  # player is either "X" or "O"
+  # symbol is the symbol to be placed
   # move is index of the target space
-  return [*board[0:move], player, *board[move::]]
+  return [*board[0:move], symbol, *board[move::]]
 
 def win(b):
     # returns winner if there is one, else returns None
-    # b is the current board state
-    if 0 not in b:
+    # b is the current board state because i don't want to type board[] a billion times
+    if blank not in b:
         return
     win_states = [
         (0, 1, 2), (3, 4, 5), (6, 7, 8), # rows
@@ -32,13 +32,13 @@ def win(b):
 def minimax(board, a, b, target, depth, maximizing):
     # returns the value of the selected branch of the game tree
     # board is the current board state
-    # a is the symbol of the current player (1 or 2)
-    # b is the symbol of the other player (1 or 2)
+    # a is the symbol of the current player
+    # b is the symbol of the other player
     # target is the spot where the symbol will be placed
     # depth is the number of recursions
     # maximizing is if we are the maximizing player or not
 
-    new_board = move_piece(board, a, target)
+    new_board = make_move(board, a, target)
 
     winner = win(new_board)
     if depth == 99 or (winner is not None):
@@ -73,18 +73,71 @@ class TicTacToe(discord.ui.View):
             description = '',
             timestamp = datetime.datetime.now()
         )
+        for i in range(9):
+            self.description += self.board[i]
+            if i%3==2:
+                self.description += '\n'
         if self.winner:
-            embed.add_field(name='', value=f'{self.winner} is the winner!')
+            embed.add_field(name='', value=f'{self.winner} was the winner!')
 
         return embed
 
-    async def send(self, message: discord.Message):
+    async def send(self, message: discord.Message): # player starts by default, but add option to pick starting player later
         self.message = await message.channel.send(embed=self.get_embed(), view=self)
 
-    @discord.ui.button(style=discord.ButtonStyle.primary, label='<', row=0)
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=0)
     async def a1(self, i: discord.Interaction, b: discord.ui.Button):
         await i.response.defer()
-        await self.update()
+        b.disabled = True
+        await self.update(0)
     
-    async def update(self):
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=0)
+    async def a2(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(1)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=0)
+    async def a3(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(2)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=1)
+    async def b1(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(3)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=1)
+    async def b2(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(4)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=1)
+    async def b3(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(5)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=2)
+    async def c1(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(6)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=2)
+    async def c2(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(7)
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, row=2)
+    async def c3(self, i: discord.Interaction, b: discord.ui.Button):
+        await i.response.defer()
+        b.disabled = True
+        await self.update(8)
+    
+    async def update(self, player_choice):
         await self.message.edit(embed=self.get_embed(), view=self)
