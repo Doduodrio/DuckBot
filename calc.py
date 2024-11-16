758afb776d8090a3dfaafc2e173c53e0d0ae6457
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-11-15 23:54:48 -0800

    Add and implement math functions

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
+import math
+
 operators = {"^": 4, "*": 3, "/": 3, "%": 3, "+": 2, "-": 2}
+functions = ["ceil", "floor", "abs", "factorial", "round", "sqrt", "log", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan"]
 
 def is_number(string):
     try:
@@ -6,9 +9,18 @@ def is_number(string):
         return True
     except:
         return False
+
+def is_string(string):
+    for letter in string:
+        if letter not in "abcdefghijklmnopqrstuvwxyz":
+            return False
+    return True
   
 def is_operator(string):
     return string in operators.keys()
+
+def is_function(string):
+    return string in functions
 
 def is_greater(a, b):
     # compares the precedence of the two operators
@@ -27,39 +39,60 @@ def shunt_yard(expression: str):
     
     # strip spaces and then convert to tokens
     for i in "".join(expression.split()):
-        if len(tokens)==0 and i in "0123456789.()^*/%+-":
+        if len(tokens)==0 and i in "abcdefghijklmnopqrstuvwxyz0123456789.()^*/%+-":
             tokens.append(i)
         elif is_number(i) or i == ".":
             if is_number(tokens[-1]) or tokens[-1] == ".":
                 tokens[-1] += i
             else:
                 tokens.append(i)
+        elif is_string(i):
+            if is_string(tokens[-1]):
+                tokens[-1] += i
+            else:
+                tokens.append(i)
         elif i in "()" or is_operator(i):
             tokens.append(i)
         else:
-            return []
+            return [] # invalid character
     
     # combine negative signs with numbers
-    i = 0
-    while i+1<len(tokens):
-        if tokens[i] == "-" and is_number(tokens[i+1]):
-            tokens[i] += tokens[i+1]
-            tokens.pop(i+1)
-        i+=1
+    j = 0
+    while j+1<len(tokens):
+        if tokens[j] == "-" and is_number(tokens[j+1]):
+            tokens[j] += tokens[j+1]
+            tokens.pop(j+1)
+        j+=1
+    
+    # substitute special mathematical constants
+    for i in range(len(tokens)-1):
+        if tokens[i+1] == "(":
+            continue
+        if tokens[i] == "pi":
+            tokens[i] = math.pi
+        elif tokens[i] == "e":
+            tokens[i] = math.e
+        elif tokens[i] == "tau":
+            tokens[i] = math.tau
+        # purposely excluding math.inf and math.nan
 
-    for thing in tokens:
-        if is_number(thing):
-            output.append(float(thing))
-        elif is_operator(thing):
-            while not (top_of(stack) == None or top_of(stack) in "()") and is_greater(top_of(stack), thing):
+    for token in tokens:
+        if is_number(token):
+            output.append(float(token))
+        elif is_function(token):
+            stack.append(token)
+        elif is_operator(token):
+            while not (top_of(stack) == None or top_of(stack) in "()") and is_greater(top_of(stack), token):
                 output.append(stack.pop())
-            stack.append(thing)
-        elif thing == "(":
-            stack.append(thing)
-        elif thing == ")":
+            stack.append(token)
+        elif token == "(":
+            stack.append(token)
+        elif token == ")":
             while not (top_of(stack) == None or top_of(stack) == "("):
                 output.append(stack.pop())
             stack.pop()
+        else:
+            return
     while len(stack) > 0:
         output.append(stack.pop())
     return output
@@ -76,21 +109,55 @@ def evaluate(expression: str):
         elif is_operator(output[0]):
             a = stack.pop()
             b = stack.pop()
-            opr = output.pop(0)
-            if opr == "^":
+            op = output.pop(0)
+            if op == "^":
                 stack.append(b ** a)
-            elif opr == "*":
+            elif op == "*":
                 stack.append(b * a)
-            elif opr == "/":
+            elif op == "/":
                 try:
                     stack.append(b / a)
                 except:
                     return
-            elif opr == "%":
+            elif op == "%":
                 stack.append(b % a)
-            elif opr == "+":
+            elif op == "+":
                 stack.append(b + a)
-            elif opr == "-":
+            elif op == "-":
                 stack.append(b - a)
+        elif is_function(output[0]):
+            a = stack.pop()
+            op = output.pop(0)
+            if op == "ceil":
+                stack.append(math.ceil(a))
+            elif op == "floor":
+                stack.append(math.floor(a))
+            elif op == "abs":
+                stack.append(abs(a))
+            elif op == "factorial":
+                stack.append(math.factorial(a))
+            elif op == "round":
+                if a%1<0.5:
+                    stack.append(math.floor(a))
+                else:
+                    stack.append(math.ceil(a))
+            elif op == "sqrt":
+                stack.append(math.sqrt(a))
+            elif op == "log":
+                stack.append(math.log(a, 10))
+            elif op == "ln":
+                stack.append(math.log(a))
+            elif op == "sin":
+                stack.append(math.sin(a))
+            elif op == "cos":
+                stack.append(math.cos(a))
+            elif op == "tan":
+                stack.append(math.tan(a))
+            elif op in ["arcsin", "asin"]:
+                stack.append(math.asin(a))
+            elif op in ["arccos", "acos"]:
+                stack.append(math.acos(a))
+            elif op in ["arctan", "atan"]:
+                stack.append(math.atan(a))
 
     return stack[0] if stack else None
