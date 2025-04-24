dfc7ce557fcdcc810546d4b01159ffe7fa48f12b
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2025-04-24 10:28:36 -0700

    Implement get_article function to return article embed

--- wiki.py
+++ wiki.py
@@ -1,9 +1,10 @@
+import datetime
+import discord
+
 from bs4 import BeautifulSoup
 import requests
 
-def get_article(article_name):
-    file = requests.get(f"https://en.wikipedia.org/wiki/{article_name}")
-    html = BeautifulSoup(file.content, features="html.parser")
+def get_article_text(html: BeautifulSoup):
     text = html.find_all(name="p")
     while html.sup:
         html.sup.extract()
@@ -11,11 +12,30 @@ def get_article(article_name):
         text[i] = ''.join(list(text[i].strings)).strip()
     return text[1]
 
-def get_image(article_name):
+def get_article_image(html: BeautifulSoup):
+    images = html.find_all(name="img")
+    for image in images:
+        if (image.has_attr("alt") and (image['alt']=="Featured article")):
+            return "https" + images[3]['src']
+        else:
+            return None
+
+def get_article(article_name):
     file = requests.get(f"https://en.wikipedia.org/wiki/{article_name}")
     html = BeautifulSoup(file.content, features="html.parser")
-    images = html.find_all(name="img")
-    if images[3].has_attr("src") and not images[3].has_attr("alt"):
-        return "https" + images[3]['src']
-    else:
-        return None
+    embed = discord.Embed(
+        color = discord.Color.dark_teal(),
+        title = article_name,
+        description = get_article_text(html),
+        url = f"https://en.wikipedia.org/wiki/{article_name}",
+        timestamp = datetime.datetime.now()
+    )
+    embed.set_thumbnail(url=get_article_image(html))
+    return embed
+
+# <img src="" decoding="async" width="250" height="275" class="mw-file-element" srcset="" data-file-width="340" data-file-height="374">
+# <img src="" decoding="async" width="190" height="250" class="mw-file-element" srcset="" data-file-width="2898" data-file-height="3807">
+# <img src="" decoding="async" width="20" height="20" class="mw-file-element" srcset="" data-file-width="512" data-file-height="512">
+# <img alt="" class="mw-file-element" data-file-height="391" data-file-width="391" decoding="async" height="27" src="" srcset="" width="27"/>
+# <img alt="Page semi-protected" src="" decoding="async" width="20" height="20" class="mw-file-element" srcset="" data-file-width="512" data-file-height="512">
+# <img alt="" class="mw-file-element" data-file-height="391" data-file-width="391" decoding="async" height="27" src="" srcset="" width="27"/>
